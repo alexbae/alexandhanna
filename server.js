45de7bddac84e5f62950073c21b60660e9ab775f
Alex Bae <alexbae84@gmail.com>
2018-11-26 00:34:40 -0800

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const guests = require('./routes/api/guests');
 
@@ -9,6 +10,15 @@ const app = express();
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -19,7 +29,7 @@ mongoose
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send(`Welcome to Alex and Hanna's website, website is under constuction now!`));
+app.get('/', (req, res) => res.send(`Hello`));
 
 // Use Guests
 app.use('/api/guests', guests);
