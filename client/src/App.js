012360fd1962bc3f64181eb4ffb777b86749f3f1
Alex Bae <alexbae84@gmail.com>
2018-11-26 00:46:01 -0800

    add const page

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,16 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Alex & Hanna</h1>
+                <h1>Alex Bae & Hanna Kim's Wedding</h1>
+                <p>Saturday, May 4 at 3:30PM to 9:00PM</p>
+                <p>
+                    <b>
+                        <a href="https://goo.gl/maps/yYZwW95uWz42" target="_blank">
+                            Catalina View Gardens
+                        </a>
+                    </b>
+                </p>
+                <p>6001 palos Verdes Drie South, Rancho Palos Verdes, CA 90275</p>
             </div>
         );
     }
