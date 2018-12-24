cf45d7d00feef998add071eadc847a140a67b2e7
Alex Bae <alexbae84@gmail.com>
2018-12-24 13:44:21 -0800

    wish me luck

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(bodyParser.json());
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
+    app.disable('etag');
+
+    app.get('/*', (req, res) => {
         res.setHeader('Last-Modified', (new Date()).toUTCString())
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
@@ -32,8 +34,6 @@ mongoose
 
 // Use Guests
 app.use('/api/guests', guests);
-
-app.disable('etag');
 
 const port = process.env.PORT || 5000;
 
