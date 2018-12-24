07641a7ffc0a1641d254f40b5308be50cd0c187d
Alex Bae <alexbae84@gmail.com>
2018-12-24 12:56:09 -0800

    add something for finger cross

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
+        res.setHeader('Last-Modified', (new Date()).toUTCString())
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
