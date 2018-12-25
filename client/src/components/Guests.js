47751a88d79c8b9822855773b6b6d8d6d9bb1797
Alex Bae <alexbae84@gmail.com>
2018-12-24 23:47:43 -0800

    add styles on guests

--- client/src/components/Guests.js
+++ client/src/components/Guests.js
@@ -31,54 +31,77 @@ class Rsvp extends Component {
     render() {
         const { guests } = this.state
 
+        const counter = side => {
+            let total = 0
+            let adults = 0
+            let kids = 0
+            
+            if (guests && guests.length) {
+                guests.map(guest => {
+                    if(guest.side === side && guest.attend) {
+                        adults += guest.adults
+                        kids += guest.kids
+                    }
+                })
+            }
+
+            total = adults + kids
+
+            return { total, kids }
+        }
+
         return (
             <section className="guest-list">
                 <div className="center">
-                    <h2>Guests List</h2>
+                    <h1 className="header">Guests List</h1>
                     <div className="guest-side">
-                        Groom's side
+                        Alex's side : total {counter('groom').total} (kids {counter('groom').kids})
                         <table>
-                            <tr>
-                                <td>Name</td>
-                                <td>Status</td>
-                                <td>Adults</td>
-                                <td>Kids</td>
-                            </tr>
-                            { guests && guests.length && (
-                                guests.map((guest, idx) =>
-                                    guest.side === 'groom' && (
-                                        <tr key={idx} className={guest.attend ? "attend" : "not-attend"}>
-                                            <td>{guest.name}</td>
-                                            <td>{guest.attend ? "attending" : "not attending"}</td>
-                                            <td>{guest.adults}</td>
-                                            <td>{guest.kids}</td>
-                                        </tr>
+                            <tbody>
+                                <tr className="tr-head">
+                                    <td>Name</td>
+                                    <td>Status</td>
+                                    <td>Adults</td>
+                                    <td>Kids</td>
+                                </tr>
+                                { guests && guests.length && (
+                                    guests.map((guest, idx) =>
+                                        guest.side === 'groom' && (
+                                            <tr key={idx} className={guest.attend ? "attend" : "not-attend"}>
+                                                <td>{guest.name}</td>
+                                                <td>{guest.attend ? "attending" : "not attending"}</td>
+                                                <td>{guest.adults}</td>
+                                                <td>{guest.kids}</td>
+                                            </tr>
+                                        )
                                     )
-                                )
-                            )}
+                                )}
+                            </tbody>
                         </table>
                     </div>
                     <div className="guest-side">
-                        Bride's side
+                        Hanna's side : total {counter('bride').total} (kids {counter('brid').kids})
                         <table>
-                            <tr>
-                                <td>Name</td>
-                                <td>Status</td>
-                                <td>Adults</td>
-                                <td>Kids</td>
-                            </tr>
-                            { guests && guests.length && (
-                                guests.map((guest, idx) =>
-                                    guest.side === 'bride' && (
-                                        <tr key={idx} className={guest.attend ? "attend" : "not-attend"}>
-                                            <td>{guest.name}</td>
-                                            <td>{guest.attend ? "attending" : "not attending"}</td>
-                                            <td>{guest.adults}</td>
-                                            <td>{guest.kids}</td>
-                                        </tr>
+                            <tbody>
+                                <tr className="tr-head">
+                                    <td>Name</td>
+                                    <td>Status</td>
+                                    <td>Adults</td>
+                                    <td>Kids</td>
+                                </tr>
+                                { guests && guests.length && (
+                                    guests.map((guest, idx) =>
+                                        guest.side === 'bride' && (
+                                            <tr key={idx} className={guest.attend ? "attend" : "not-attend"}>
+                                                <td>{guest.name}</td>
+                                                <td>{guest.attend ? "attending" : "not attending"}</td>
+                                                <td>{guest.adults}</td>
+                                                <td>{guest.kids}</td>
+                                            </tr>
+                                        )
                                     )
-                                )
-                            )}
+                                )}
+                            </tbody>
                         </table>
                     </div>
                 </div>
